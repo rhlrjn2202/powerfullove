558ab734d2e04a5d592449ae536573323421d7e8
rhlrjn2202 <46061702+rhlrjn2202@users.noreply.github.com>
2024-11-13 01:30:59 +0530

    commit 11

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,48 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/admin");
+
+  // Date filters
+  eleventyConfig.addFilter("dateIso", date => {
+    return DateTime.fromJSDate(date).toISO();
+  });
+
+  eleventyConfig.addFilter("dateDisplay", date => {
+    return DateTime.fromJSDate(date).toLocaleString(DateTime.DATE_FULL);
+  });
+
+  // Sort by date
+  eleventyConfig.addFilter("sortByDate", arr => {
+    return arr.sort((a, b) => new Date(b.data.date) - new Date(a.data.date));
+  });
+
+  // Excerpt filter
+  eleventyConfig.addFilter("excerpt", content => {
+    if (!content) return '';
+    const excerptLength = 200;
+    let excerpt = content.replace(/(<([^>]+)>)/gi, ''); // Remove HTML tags
+    excerpt = excerpt.substr(0, excerptLength);
+    if (excerpt.length === excerptLength) {
+      excerpt += '...';
+    }
+    return excerpt;
+  });
+
+  // Collections
+  eleventyConfig.addCollection("blog", collection => {
+    return collection.getFilteredByGlob("src/blog/*.md");
+  });
+
+  eleventyConfig.addCollection("events", collection => {
+    return collection.getFilteredByGlob("src/events/*.md");
+  });
+
+  eleventyConfig.addCollection("teachings", collection => {
+    return collection.getFilteredByGlob("src/teachings/*.md");
+  });
   
   return {
     dir: {

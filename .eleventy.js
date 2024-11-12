b0270b889e060cd5e10bc6171b4a3b737f9414a4
rhlrjn2202 <46061702+rhlrjn2202@users.noreply.github.com>
2024-11-13 00:29:25 +0530

    commit 8

--- .eleventy.js
+++ .eleventy.js
@@ -1,44 +1,16 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
-
 module.exports = function(eleventyConfig) {
-  // Plugins
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  
-  // Pass through copy
   eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("src/admin");
-  
-  // Custom filters
-  eleventyConfig.addFilter("limit", function(array, limit) {
-    return array.slice(0, limit);
-  });
-
-  // Collection for events
-  eleventyConfig.addCollection("events", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/events/*.md");
-  });
-  
-  // Markdown config
-  let markdownLibrary = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true
-  }).use(markdownItAnchor);
-  
-  eleventyConfig.setLibrary("md", markdownLibrary);
+  eleventyConfig.addPassthroughCopy("src/css");
   
   return {
     dir: {
       input: "src",
       output: "_site",
       includes: "_includes",
-      data: "_data"
+      layouts: "_layouts"
     },
-    templateFormats: ["md", "njk", "html"],
-    markdownTemplateEngine: "njk",
+    templateFormats: ["njk", "md"],
     htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk"
+    markdownTemplateEngine: "njk"
   };
 };
